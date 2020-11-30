e0045f94a45bd45552d24e26a3936d4a411b8f23
Waliff Cordeiro <waliff.cordeiro@gmail.com>
2020-11-30 13:36:33 -0300

    Criando estrutura da tradução

---
+++ include/traducao.hpp
@@ -0,0 +1,10 @@
+#ifndef TRADUCAO_H
+#define TRADUCAO_H
+
+#include "classes.hpp"
+
+vector<string> traducao(vector<string> *text, vector<string> *bss, vector<string> *data);
+
+void print_traducao(vector<string> traducao, string nome_arquivo);
+
+#endif /* TRADUCAO_H */
---
+++ source/traducao.cpp
@@ -0,0 +1,11 @@
+#include "../include/traducao.hpp"
+
+
+vector<string> traducao(vector<string> *text, vector<string> *bss, vector<string> *data) {
+    vector<string> aux;
+    return aux;
+}
+
+void print_traducao(vector<string> traducao, string nome_arquivo) {
+    
+}
