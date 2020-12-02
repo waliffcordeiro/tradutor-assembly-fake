f3d0bb8d51cee71fab6c2f07128dc2c3e5f3ce23
Waliff Cordeiro <waliff.cordeiro@gmail.com>
2020-12-02 16:22:44 -0300

    Refatorando Escrita e Leitura

--- assembly-code/IO_functions.asm
+++ assembly-code/IO_functions.asm
@@ -66,134 +66,130 @@ EscreverString:
     ret
 
 LerInteiro:
-    enter 0,0
-    push ebx
-    push ecx
-    push edx
-    push edi
+  enter 0,0
+  push ebx
+  push ecx
+  push edx
+  push edi
 
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, aux
-    mov edx, aux_size
-    int 80h
+  mov eax, 3
+  mov ebx, 0
+  mov ecx, aux
+  mov edx, aux_size
+  int 80h
 
-    xor ecx, ecx
-    xor eax, eax
-    mov ebx, aux
+  xor ecx, ecx
+  xor eax, eax
+  mov ebx, aux
 
-LerInteiro_Loop:
-    mov dl, 0x0a
-    cmp dl, [ebx + ecx]
-    je Ler_Cont
-    push eax
-    shl eax, 3
-    add eax, [esp]
-    add eax, [esp]
-    mov dl, 0x2d
-    cmp dl, [ebx + ecx]
-    je Ler_Negativo
-    sub edx, edx
-    mov dl, [ebx + ecx]
-    sub edx, 0x30
-    add eax, edx
-
-Ler_Negativo:
-    inc ecx
-    jmp LerInteiro_Loop
-
-Ler_Cont:
-    mov dl, 0x2d
-    cmp byte dl, [ebx]
-    jne Ler_Positivo
-    push eax
-    xor eax, eax
-    sub eax, [esp]
-    add esp, 4
-
-Ler_Positivo:
-    pop edi
-    pop edx
-    pop ecx
-    pop ebx
-    leave
-    ret
+Leitura_Conv_Loop:
+  mov dl, 0x0a
+  cmp dl, [ebx + ecx]
+  je Leitura_Cont
+  push eax
+  shl eax, 3
+  add eax, [esp]
+  add eax, [esp]
+  mov dl, 0x2d
+  cmp dl, [ebx + ecx]
+  je Leitura_Sinal_Negativo
+  sub edx, edx
+  mov dl, [ebx + ecx]
+  sub edx, 0x30
+  add eax, edx
+Leitura_Sinal_Negativo:
+  inc ecx
+  jmp Leitura_Conv_Loop
+Leitura_Cont:
+  mov dl, 0x2d
+  cmp byte dl, [ebx]
+  jne Leitura_Sinal_Positivo
+  push eax
+  xor eax, eax
+  sub eax, [esp]
+  add esp, 4
+Leitura_Sinal_Positivo:
+  pop edi
+  pop edx
+  pop ecx
+  pop ebx
+  leave
+  ret
 
 EscreverInteiro:
-    enter 0,0
-    pusha
+  enter 0,0
+  pusha
 
-    xor eax, eax
-    xor edx, edx
-    xor ecx, ecx
-    xor edi, edi
-    mov ebx, aux
-    mov eax, [EBP + 8]
+  xor eax, eax
+  xor edx, edx
+  xor ecx, ecx
+  xor edi, edi
+  mov ebx, aux
+  mov eax, [EBP + 8]
 
-    cmp eax, 0
-    jne Print_Negativo
-    mov dl, 0
-    add dl, 0x30
-    mov byte [ebx], dl
-    mov dl, [ebx]
-    inc ecx
-    jmp Print_Cont3
+  cmp eax, 0
+  jne Escrita_Sinal_Negativo
+  mov dl, 0
+  add dl, 0x30
+  mov byte [ebx], dl
+  mov dl, [ebx]
+  inc ecx
+  jmp Escrita_Cont3
 
-Print_Negativo:  
-    test eax, 0x80000000
-    je Print_Positivo
-    xor eax, eax
-    sub eax, [EBP + 8]
-    mov byte [ebx], 0x2d 
-    inc ebx
-    mov edi, 1
-    jmp Print_Loop
+Escrita_Sinal_Negativo:  
+  test eax, 0x80000000
+  je Escrita_Sinal_Positivo
+  xor eax, eax
+  sub eax, [EBP + 8]
+  mov byte [ebx], 0x2d 
+  inc ebx
+  mov edi, 1
+  jmp Escrita_Conv_Loop
 
-Print_Positivo:
-    mov eax, [EBP + 8]
+Escrita_Sinal_Positivo:
+  mov eax, [EBP + 8]
 
-Print_Loop:
-    xor edx, edx
-    push ebx
-    mov ebx, 10
-    div ebx
-    pop ebx
+Escrita_Conv_Loop:
 
-    cmp eax, 0
-    jne Print_Cont
-    cmp edx, 0
-    je Print_Cont3
+  xor edx, edx
+  push ebx
+  mov ebx, 10
+  div ebx
+  pop ebx
 
-Print_Cont:  
-    add edx, 0x30
+  cmp eax, 0
+  jne Escrita_Cont
+  cmp edx, 0
+  je Escrita_Cont3
 
-    push ecx
+Escrita_Cont:  
+  add edx, 0x30
 
-PI32_Shift_R:
-    cmp ecx, 0
-    je Print_Cont2
-    mov byte dh, [ebx + ecx - 1]
-    mov byte [ebx + ecx], dh 
-    dec ecx
-    jmp PI32_Shift_R
+  push ecx
+Escrita_Shift_Direita:
+  cmp ecx, 0
+  je Escrita_Cont2
+  mov byte dh, [ebx + ecx - 1]
+  mov byte [ebx + ecx], dh 
+  dec ecx
+  jmp Escrita_Shift_Direita
+Escrita_Cont2:  
+  mov byte [ebx], dl
+  pop ecx
+  inc ecx
+  jmp Escrita_Conv_Loop
 
-Print_Cont2:  
-    mov byte [ebx], dl
-    pop ecx
-    inc ecx
-    jmp Print_Loop
+Escrita_Cont3:
+  cmp edi, 1
+  jne Print_Final
+  inc ecx
+  dec ebx
 
-Print_Cont3:
-    cmp edi, 1
-    jne Inteiro_Print
-    inc ecx
-    dec ebx
-
-Inteiro_Print:  
-    push ebx
-    push ecx
-    call Print_String
-    add esp, 8
-    popa
-    leave
-    ret
+Print_Final:  
+  push ebx
+  push ecx
+  call EscreverString
+  add esp, 8
+  popa
+  leave
+  ret
